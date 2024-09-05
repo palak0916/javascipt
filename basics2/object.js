dca9fa589e043211337e3902024025e5cb6d8b04
palak0916 <palakbabber0916@gmail.com>
2024-09-05 18:03:27 +0000

    object methods

--- basics2/object.js
+++ basics2/object.js
@@ -1,6 +1,6 @@
 //declaring object literals
-const mySym= Symbol("key1")
-const obj={
+//const mySym= Symbol("key1")
+/*const obj={
     name:"palak",
     city:"patiala",
     hobby:"dance",
@@ -16,14 +16,53 @@ const obj={
 //Object.freeze(obj)
 //obj.city="palki"
 //console.log(obj);
-obj.greeting= function() {
-    console.log("hello");
+//obj.greeting= function() {
+   // console.log("hello");
 }
-obj.greetingtwo= function() {
-    console.log(`hello,${this.hobby}`);
+//obj.greetingtwo= function() {
+    //console.log(`hello,${this.hobby}`);
 }
-console.log(obj.greeting());
-console.log(obj.greetingtwo());
+//console.log(obj.greeting());
+//console.log(obj.greetingtwo());*/
 
+//2declaring objects using sinleton and non singleton(constructor)
 
-
+//const tinderuser= new Object();//singleton
+//console.log(tinderuser);
+const tinderuser={}
+tinderuser.email="palakbabber@gmail.com"
+tinderuser.city="patiala"
+tinderuser.loggedin="false"
+ /*console.log(Object.values(tinderuser));
+ //console.log(Object.entries(tinderuser));
+ console.log(Object.keys(tinderuser));
+ console.log(tinderuser.hasOwnProperty('loggedin'));*/
+ const regularuser={
+       city:"patiala",
+         user:{
+             fullname:{
+               firstname:"palak",
+                lastname:"babber"
+                                }
+              }
+        }
+        //console.log(regularuser.user.fullname); //here ? is for to tell us that the object is present here or  not in case it is not present
+      
+        //console.log(regularuser.user?.fullname);   
+        const users = [
+            {
+                id: 1,
+                email: "h@gmail.com"
+            },
+            {
+                id: 1,
+                email: "h@gmail.com"
+            },
+            {
+                id: 1,
+                email: "h@gmail.com"
+            },
+        ]
+        
+        users[1].email
+                     
